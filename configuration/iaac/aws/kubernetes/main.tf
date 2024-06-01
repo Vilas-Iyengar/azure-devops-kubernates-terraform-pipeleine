982ebc557c4ee8879515d3bb3f590bd932803ac2
VishwasIyengar <167862661+VishwasIyengar@users.noreply.github.com>
2024-06-01 16:29:10 +0530

    Update main.tf

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -17,96 +17,50 @@ resource "aws_default_vpc" "default" {
 
 }
 
-data "aws_eks_cluster" "example" {
-   name = "in28minutes-cluster"
- }
-
-data "aws_eks_cluster_auth" "example" {
-  name = "in28minutes-cluster"
+data "aws_subnet_ids" "subnets" {
+  vpc_id = aws_default_vpc.default.id
 }
 
-#Get token to connect to Kubernetes
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.example.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.example.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.example.token
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  version                = "~> 2.12"
 }
-
 
 module "in28minutes-cluster" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.3"
-
+  source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "in28minutes-cluster"
-  cluster_version = "1.29"
-
-  subnet_ids         = ["subnet-0ba16627", "subnet-02db6b4a", "subnet-e08479ba"] #CHANGE # Donot choose subnet from us-east-1e
+  cluster_version = "1.14"
+  subnets         = ["subnet-01f4d462b9bb3d5af", "subnet-0a1514a0b43e11758"] #CHANGE
   #subnets = data.aws_subnet_ids.subnets.ids
   vpc_id          = aws_default_vpc.default.id
+
   #vpc_id         = "vpc-1234556abcdef"
 
-  //Newly added entry to allow connection to the api server
-  //Without this change error in step 163 in course will not go away
-  cluster_endpoint_public_access  = true
-
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
-  }
-
-  eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
+  node_groups = [
+    {
+      instance_type = "t2.micro"
+      max_capacity  = 5
+      desired_capacity = 3
+      min_capacity  = 3
     }
-
-    two = {
-      name = "node-group-2"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
-  }
-
+  ]
 }
 
-resource "kubernetes_cluster_role_binding" "example" {
-  metadata {
-    name = "fabric8-rbac"
-  }
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-  }
-  subject {
-    kind      = "ServiceAccount"
-    name      = "default"
-    namespace = "default"
-  }
+data "aws_eks_cluster" "cluster" {
+  name = module.in28minutes-cluster.cluster_id
 }
 
-# Create a secret. After version 1.23 there is no default secret
-resource "kubernetes_secret" "example" {
-  metadata {
-    annotations = {
-      "kubernetes.io/service-account.name" = "default"
-    }
-
-    generate_name = "terraform-default-"
-  }
-
-  type                           = "kubernetes.io/service-account-token"
-  wait_for_service_account_token = true
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.in28minutes-cluster.cluster_id
 }
+
+
+# We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
+# ServiceAccount needs permissions to create deployments 
+# and services in default namespace
+
 
 # Needed to set the default region
 provider "aws" {
